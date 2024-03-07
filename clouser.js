bc52628d8852b247febe67357fc7adaa6763d6a6
Ramchandani Dheearj <dheerajramchandani24@gamil.com>
2024-03-07 17:49:39 +0530

    fruit

---
+++ clouser.js
@@ -0,0 +1,10 @@
+// function outer(){
+// var str ="i am outer function";
+// function inner(){
+//     console.log(str)
+// }
+// return inner;
+// }
+// clouser=outer();
+// clouser()
+
--- forEach-map.js
+++ forEach-map.js
@@ -1,10 +1,27 @@
-let arr=[1,2,3,4]
-let z=arr.forEach((a) => {
-    console.log(a);
-});
-
 // let arr=[1,2,3,4]
+// let z=arr.forEach((a) => {
+//     console.log(a);
+// });
+
+// let arr=[1,2,3,4].
 // let z=arr.map((a) => {
 // return a*2;
 // });
 // console.log(z);
+// function add(){
+//     console.log("hello");
+//     function sub(){
+//         console.log("hello2");
+//     }
+//     return sub
+// }
+// // add()
+// console.log(add());
+arr=[1,2,3,4,5,6,7,8,9,10];
+// function table() {
+// for (let a = 1; a <=arr.length; a++) {
+//   console.log(a*2);   
+// }
+// }
+// table();
+ numb = arr.map((num)=>console.log(num*2))
